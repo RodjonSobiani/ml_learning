3e2e76ae1e16cd4be4c3f4fa4192e0c51dcddf79
Rodjon Sobiani <RodjonSobiani@GMail.com>
2022-05-30 22:12:05 +0700

    basic python

---
+++ python_basic/2. variables_and_types.py
@@ -0,0 +1,11 @@
+myString = 'hello'
+myFloat = float(10)
+myInt = 20
+
+# testing code
+if myString == "hello":
+    print("String: %s" % myString)
+if isinstance(myFloat, float) and myFloat == 10.0:
+    print("Float: %f" % myFloat)
+if isinstance(myInt, int) and myInt == 20:
+    print("Integer: %d" % myInt)
---
+++ python_basic/5. string_formatting.py
@@ -0,0 +1,6 @@
+# Hello John Doe. Your current balance is $53.44.
+
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is $%.2f."
+
+print(format_string % data)
