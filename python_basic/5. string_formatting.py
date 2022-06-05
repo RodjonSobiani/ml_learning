9d9232ec083e819e9fca202fa1bfae738245baea
Rodjon Sobiani <RodjonSobiani@GMail.com>
2022-06-05 22:13:21 +0700

    fix exercise #5 (float to string)

--- python_basic/5. string_formatting.py
+++ python_basic/5. string_formatting.py
@@ -1,4 +1,4 @@
 data = ("John", "Doe", 53.44)
-format_string = "Hello %s %s. Your current balance is $%.2f."
+format_string = "Hello %s %s. Your current balance is $%s."
 
 print(format_string % data)
