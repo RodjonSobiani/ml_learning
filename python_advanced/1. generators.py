5f609aea0b1a190c77c0a105498b9517790d1ccb
RodjonSobiani <rodjonsobiani@gmail.com>
2022-06-06 14:04:28 +0700

    add generators exercise

---
+++ python_advanced/1. generators.py
@@ -0,0 +1,22 @@
+import types
+
+
+def fib():
+    a = 0
+    b = 1
+    while True:
+        c = b + a
+        b = a
+        a = c
+        yield c
+
+
+if type(fib()) == types.GeneratorType:
+    print("Good, The fib function is a generator.")
+
+    counter = 0
+    for n in fib():
+        print(n)
+        counter += 1
+        if counter == 10:
+            break
