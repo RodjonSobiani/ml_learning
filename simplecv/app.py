bc56964df5d3ecac247cbdd7343cc075002fee21
RodjonSobiani <rodjonsobiani@gmail.com>
2022-06-22 13:40:33 +0700

    Done flask.app image binary classification.

--- simplecv/app.py
+++ simplecv/app.py
@@ -1,38 +1,29 @@
-# from flask import (
-#     Blueprint, render_template
-# )
-#
-# from .__init__ import create_app
-#
-# create_app()
-#
-# bp = Blueprint('app', __name__)
-#
-#
-# @bp.route('/')
-# def index():
-#     return render_template('app/index.html')
-
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from keras.models import load_model
-from keras.preprocessing import image
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
-
-dic = {0: 'Cat', 1: 'Dog'}
 
 model = load_model('final_model.h5')
 
 model.make_predict_function()
 
 
-def predict_label(img_path):
-    i = image.load_img(img_path, target_size=(100, 100))
-    i = image.img_to_array(i) / 255.0
-    i = i.reshape(1, 100, 100, 3)
-    p = model.predict_classes(i)
-    return dic[p[0]]
+def predictImage(img_path):
+    img1 = tf.keras.utils.load_img(img_path, target_size=(150, 150))
+    plt.imshow(img1)
+    Y = tf.keras.utils.img_to_array(img1)
+    X = np.expand_dims(Y, axis=0)
+    val = model.predict(X)
+    if val == 1:
+        # return plt.xlabel("Пёсика", fontsize=30)
+        return "Пёсика"
+    elif val == 0:
+        # return plt.xlabel("Котика", fontsize=30)
+        return "Котика"
+    return (val)
 
 
 # routes
@@ -41,19 +32,18 @@ def main():
     return render_template("index.html")
 
 
-@app.route("/submit", methods=['GET', 'POST'])
+@app.route("/predicter", methods=['GET', 'POST'])
 def get_output():
+    global img_path, p
     if request.method == 'POST':
         img = request.files['my_image']
-
         img_path = "static/uploads/" + img.filename
         img.save(img_path)
-
-        p = predict_label(img_path)
-
+        p = predictImage(img_path)
+    if request.method == 'GET':
+        return redirect("/")
     return render_template("index.html", prediction=p, img_path=img_path)
 
 
 if __name__ == '__main__':
-    # app.debug = True
     app.run(debug=True)
